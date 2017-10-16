f86043371605006f78bd8b44ebd3fcdf25c16d97
TalAizikov <31697771+TalAizikov@users.noreply.github.com>
2017-10-16 09:35:07 -0400

    Create Challenge: Random blobber.js

---
+++ Challenge: Random blobber.js
@@ -0,0 +1,35 @@
+var Walker = function() {
+    this.x = width/2;
+    this.y = height/2;
+};
+
+Walker.prototype.display = function() {
+    noStroke();
+    fill(176, 0, 0);
+    ellipse(this.x, this.y, 10, 10);
+};
+
+// Randomly move right, left, down, or up
+Walker.prototype.walk = function() {
+    var choice = floor(random(4));
+    if (choice === 0) {
+        //move right
+        this.x += 5;
+    } else if (choice === 1) {
+        //move left
+        this.x -= 5;
+    } else if (choice === 2) {
+        //move down
+        this.y += 5;
+    } else {
+        //move up
+        this.y -= 5;
+    } 
+};
+
+var w = new Walker();
+
+draw = function() {
+    w.walk();
+    w.display();
+};
